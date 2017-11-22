e9db63bc7298a5795ea26c5f25e0d05cd86fdd3d
Pollarwat B <32667569+rorocrocky@users.noreply.github.com>
2017-11-23 02:44:44 +0700

    New
    Video intro with faster loading

---
+++ ITS488_vrgameproject/Assets/Rocky/Script/CountdownTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour {
+	public string levelToLoad;
+	private float timer = 8f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if(timer <= 0)
+		{
+			Application.LoadLevel(levelToLoad);
+	}
+}
+}
+
---
+++ ITS488_vrgameproject/Assets/Rocky/Script/LevelControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelControl : MonoBehaviour {
+	
+	public int index;
+	public string levelName;
+	
+	public Image black;
+	public Animator anim;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			StartCoroutine(Fading());
+			//SceneManager.LoadScene(1);
+		}
+	}
+	
+	IEnumerator Fading()
+	{
+		anim.SetBool("Fade",true);
+		yield return new WaitUntil(()=>black.color.a==1);
+		SceneManager.LoadScene(index);
+	}
+}
